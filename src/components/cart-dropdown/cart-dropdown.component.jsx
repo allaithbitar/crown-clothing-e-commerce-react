ac7885f36c415e963d84a6e5887496f8eb91ee34
AllaithBitarCode0 <allaithbitar.code0@gmail.com>
2021-11-01 01:01:20 +0200

    updating the app with memomized redux selectors for performance

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      <CustomButton>GO TO CHECKOUT</CustomButton>
+      {cartItems.map((cartItem) => (
+        <CartItem item={cartItem} />
+      ))}
     </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+});
 
-export default CartDropDown;
+export default connect(mapStateToProps)(CartDropDown);
